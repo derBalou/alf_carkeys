20c006d7a9e79a3eb612bece0f4bb72d2b95ce47
derBalou <73936779+derBalou@users.noreply.github.com>
2021-07-21 16:23:13 +0200

    Update vehicle_keys.sql

--- Alf-carkeys/vehicle_keys.sql
+++ Alf-carkeys/vehicle_keys.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `vehicle_keys` (
   `id` int(11) NOT NULL,
-  `identifier` varchar(22) NOT NULL,
+  `identifier` longtext NOT NULL,
   `plate` varchar(12) NOT NULL,
   `state` longtext,
   `label` longtext
@@ -11,7 +11,4 @@ ALTER TABLE `vehicle_keys`
 
 ALTER TABLE `vehicle_keys`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-  
-ALTER TABLE `vehicle_keys` CHANGE `identifier` `identifier` VARCHAR(41) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL; 
-
 COMMIT;
